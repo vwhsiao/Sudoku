51a7cbd64fcaa24bcbe74e001f9fc93db2dc0e0c
Faye Jao <yjao@uci.edu>
2016-03-17 19:11:22 -0700

    square done

--- Sudoku/Sudoku/Square.h
+++ Sudoku/Sudoku/Square.h
@@ -3,50 +3,58 @@
 
 #include <vector>
 #include <string>
+
+
 class Square
 {
 public:
-	Square(int row, int col, int value, int boxH, int boxW);
-	Square(int boxH, int boxW, int boxNum);
-	~Square();
-	void print();
 
+	// Constructors and Destructor
+	Square(int row, int col, int value, int boxH, int boxW);
+	Square(int row, int col, int boxNum, int value, std::vector<int> domain, std::vector<int> storedDomain);
+	~Square();
+
+	// Domains
 	void initDomain(int size);
 	void removeFromDomain(int _value=-1);
 	void addToDomain(int _value);
+	std::vector<int> getDomain();
+	void restoreDomain();
+	void restoreDomains(std::vector<int> domain, std::vector<int> storedDomain);
+	void storeDomain();
+	std::vector<int> getStoredDomain();
+	void preserveDomains(int _value);
+
+	// Value
 	void resetValue();
 	void setValue(int newValue);
 	int getValue();
-	
-	std::string getDomainString(bool showLastResult=false);
+
+	// Prints and Strings
+	void print();
 	void printDomain();
-	std::vector<int> getDomain();
-
-	std::vector<int> initialDomain;
-	void restoreDomain();
-	void storeDomain();
-
-	int row, col, boxH, boxW;
-	int boxNum;
-private:
-	std::vector<int> domain;
-	std::vector<int> storedDomain;
-	int value;
-
-public:
-	bool given = false;
-	std::string lastAddingAttempt = "";
-	std::vector<Square> neighborInfos = std::vector<Square>();
-	Square(int row, int col, int boxNum, int value, std::vector<int> domain, std::vector<int> storedDomain);
+	std::string getDomainString(bool showLastResult = false);
+	std::string getPreservedDomainsString();
 	std::string getNeighborInfosString();
 	std::string getHostString();
-	void restoreDomains(std::vector<int> domain, std::vector<int> storedDomain);
+	std::string lastAddingAttempt = "";
+
+	// Square Properties
 	bool domainLocked = false;
-	std::vector<int> getStoredDomain();
-	std::vector<Square*> neighborReferences;
+	bool given = false;
+	int value;
+	int row, col, boxH, boxW;
+	int boxNum;
+
+	// Domain Information
+	std::vector<int> initialDomain;
+	std::vector<int> domain;
+	std::vector<int> storedDomain;
 	std::vector<std::vector<int>> domainsForPrevHost = std::vector<std::vector<int>>();
-	void preserveDomains(int _value);
-	std::string getPreservedDomainsString();
+	
+	// Neighbor Informaion
+	std::vector<Square*> neighborReferences;
+	std::vector<Square> neighborInfos = std::vector<Square>();	
 };
 
 #endif
